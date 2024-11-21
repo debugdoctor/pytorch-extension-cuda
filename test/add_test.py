51ac4dbdd994647abb51419e04a1fb2bfa327bc7
debugdoctor <aime_rick@163.com>
2024-11-21 15:54:04 +0800

    fix: test file fixing

--- test/add_test.py
+++ test/add_test.py
@@ -2,9 +2,13 @@ import torch
 import torch_tools
 
 if __name__ == "__main__":
-    a = torch.randn(4,4)
-    b = torch.randn(4,4)
+    # huggingface parallelism : auto device_map is supported
+    a = torch.randn(4,4).cuda(1)
+    b = torch.randn(4,4).cuda(1)
+    c = torch.randn(4,4).cuda(2)
 
     print(a)
     print(b)
+    print(c)
     print(torch_tools.add_two(a, b))
+    print(torch_tools.add_two(b.cuda(2), c))
